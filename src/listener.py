a7af92c1764a2fac9e8796e9f80ea6d63ea9b808
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-09-13 21:35:20 -0500

    removed plugins. Going to be handled in pip

---
+++ src/listener.py
@@ -0,0 +1,27 @@
+import types
+
+
+class Listener(object):
+    def __init__(self, robot, matcher, callback=None):
+        """
+        Listeners receive every messsage from the chat source and 
+        decide if they want to act on it
+
+        An identifier should be provided in the options parameter 
+        to uniquely identify the listener (options.id)
+
+        robot    - A Robot instance
+        matcher  - A function that determines if this listener 
+        should trigger the callback
+
+        options  - an object of additional parameters keyed on 
+        extension name (optional)
+
+        callback - a function that is triggered if the message matches
+        """
+        self.bot = robot
+        self.matcher = matcher
+
+    def call(self, message):
+        pass
+
