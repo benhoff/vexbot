dd23d437d6be2285ae54c5a6f9d7f3edf03da7a6
Ben Hoff <hoff.benjamin.k@gmail.com>
2016-06-25 21:14:34 -0600

    added in commands directory

---
+++ vexbot/adapters/commands/call_editor.py
@@ -0,0 +1,33 @@
+import os
+
+
+def call_editor(directory=None):
+    editor = os.environ.get('EDITOR', 'vim')
+    initial_message = b""
+    file = None
+    filename = None
+
+    if directory is not None:
+        # create a random filename
+        filename = ''.join(random.choice(string.ascii_uppercase +
+                                         string.digits) for _ in range(8))
+
+        filename = filename + '.py'
+        # TODO: loop back if isfile is True
+        filepath = os.path.join(directory, filename)
+        file = open(filepath, 'w+b')
+    else:
+        file = tempfile.NamedTemporaryFile(prefix=directory, suffix='.py')
+        filename = file.name
+    current_dir = os.getcwd()
+    os.chdir(directory)
+    file.write(initial_message)
+    file.flush()
+    call([editor, filename])
+    file.close()
+    file = open(filename)
+    message = file.read()
+    file.close()
+    os.chdir(current_dir)
+
+    return message
