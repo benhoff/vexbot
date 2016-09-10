07c04abd1c4c56ebb5e5de59ff147c595e9ba746
Ben Hoff <hoff.benjamin.k@gmail.com>
2016-09-09 21:00:01 -0600

    moved vedir to `.config` in home directory

--- vexbot/commands/create_vexdir.py
+++ vexbot/commands/create_vexdir.py
@@ -1,14 +1,26 @@
 from os import path, mkdir
 
 
-def get_vexdir_filepath():
+def _get_config_dir():
     home_direct = path.expanduser("~")
-    vexdir = path.abspath(path.join(home_direct, '.vexbot'))
+    config = path.abspath(path.join(home_direct,
+                                    '.config'))
+
+    return config
+
+def get_vexdir_filepath():
+    vexdir = path.join(_get_config_dir(),
+                       'vexbot')
+
     return vexdir
 
 
 def create_vexdir():
+    config_dir = _get_config_dir()
     vexdir = get_vexdir_filepath()
+
+    if not path.isdir(config_dir):
+        mkdir(config_dir)
     if not path.isdir(vexdir):
         mkdir(vexdir)
 
