3b808112b6d212d9d51e026490b0b7f3a66049ce
Ben Hoff <hoff.benjamin.k@gmail.com>
2017-01-27 19:25:52 -0700

    added in comments

--- vexbot/messaging.py
+++ vexbot/messaging.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import logging
+import traceback
 from contextlib import ContextDecorator
 
 import zmq
@@ -17,73 +18,87 @@ class Messaging:
                  subscribe_address=None,
                  heartbeat_address=None):
 
+        # NOTE: currently not used
         self._service_name = profile 
+        # Store the addresses of publish, subscription, and heartbeat sockets
         self.address = {'publish': publish_address,
                         'subscriptions': subscribe_address,
                         'heartbeat': heartbeat_address}
 
-        self.subscription_socket = None
-        self.publish_socket = None
-
-        # NOTE: Since Sub & Pub socket managed by robot
-        # NOT tracked here in self.socket
+        # The XPub and xSub sockets acts as a pass through
+        # they are stored in `socket` since we don't need access to them
         self.socket = {'publish': None,
                        'subscribe': None,
                        'heartbeat': None}
 
+        # The subscription socket and publish socket are what the robot uses
+        # to communicate. Separate from the proxy XPUB/XSUB
+        self.subscription_socket = None
+        self.publish_socket = None
+
+        # The poller polls each socket
         self._poller = zmq.Poller()
         self.running = False
+        self._logger = logging.getLogger(__name__)
 
     def start(self, zmq_context=None):
+        """
+        starts/instantiates the messaging
+        """
         context = zmq_context or zmq.Context()
 
         # Publish socket managed by robot
         self.publish_socket = context.socket(zmq.PUB)
 
-        self._poller = zmq.Poller()
-
+        # Currently reading all messages
         self.subscription_socket = context.socket(zmq.SUB)
         self.subscription_socket.setsockopt(zmq.SUBSCRIBE, b'')
         self._poller.register(self.subscription_socket, zmq.POLLIN)
 
         # XPUB socket, IN type
         pub = context.socket(zmq.XPUB)
+        self._poller.register(pub, zmq.POLLIN)
+        self.socket['publish'] = pub
+
         addresses = self.address['subscriptions']
 
         if addresses:
+            # Can subscribe to multiple addresses
             for addr in addresses:
                 try:
                     pub.bind(addr)
                 except zmq.error.ZMQError:
-                    pass
+                    s = 'Address bind attempt fail. Address tried: {}'
+                    s.format(addr)
+                    self._logger.error(s)
 
                 # NOTE: still in the for loop here
                 self.subscription_socket.connect(addr)
 
-        self.socket['publish'] = pub
-        self._poller.register(pub, zmq.POLLIN)
-
         # XSUB socket, IN type
         sub = context.socket(zmq.XSUB)
+        self._poller.register(sub, zmq.POLLIN)
+        self.socket['subscribe'] = sub
+
         sub_addr = self.address['publish']
         if sub_addr:
             try:
                 sub.bind(sub_addr)
             except zmq.error.ZMQError:
+                self._logger.error('publish address failed. Exiting bot.')
                 sys.exit(1)
             self.publish_socket.connect(sub_addr)
 
-        self.socket['subscribe'] = sub
-        self._poller.register(sub, zmq.POLLIN)
 
         # heartbeat
         heartbeat = context.socket(zmq.ROUTER)
+        self._poller.register(heartbeat, zmq.POLLIN)
+        self.socket['heartbeat'] = heartbeat
+
         hb_addr = self.address['heartbeat']
         if hb_addr:
             heartbeat.bind(hb_addr)
 
-        self.socket['heartbeat'] = heartbeat
-        self._poller.register(heartbeat, zmq.POLLIN)
 
     def run(self):
         self.running = True
@@ -93,7 +108,6 @@ class Messaging:
 
         while True:
             socks = dict(self._poller.poll(timeout=500))
-            socks = dict(socks)
             if sub in socks:
                 try:
                     msg = sub.recv_multipart(zmq.NOBLOCK)
@@ -119,7 +133,8 @@ class Messaging:
                     msg = self.subscription_socket.recv_multipart(zmq.NOBLOCK)
                     try:
                         msg = decode_vex_message(msg)
-                    except Exception:
+                    # TODO: error handeling
+                    except Exception as e:
                         pass
                     else:
                         yield msg
