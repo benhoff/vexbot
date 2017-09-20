5b5b27611850aad79e3eb44e326840a94500ed20
Ben Hoff <hoff.benjamin.k@gmail.com>
2017-09-20 04:20:31 +0000

    added return type to function call

--- vexbot/util/messaging.py
+++ vexbot/util/messaging.py
@@ -1,4 +1,4 @@
-def get_address(message: list):
+def get_addresses(message: list) -> list:
     """
     parses a raw list from zmq to get back the components that are the address
     messages are broken by the addresses in the beginning and then the
