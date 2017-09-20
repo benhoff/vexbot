3afb880afea5031e3a5e0c26f824b5f90e0ad12d
Ben Hoff <hoff.benjamin.k@gmail.com>
2017-09-20 05:02:42 +0000

    fix imports on subprocess manager

--- vexbot/subprocess_manager.py
+++ vexbot/subprocess_manager.py
@@ -1,13 +1,11 @@
-import sys
-import atexit
-import signal
-import logging
+# import atexit
+# import signal
+
 # TODO: Determine if there's a better way to import this
-from gi._error import GError
+from gi._error import GError as _GError
 
-from pydbus import SessionBus, SystemBus
-
-import pluginmanager
+from pydbus import SessionBus as _SessionBus
+from pydbus import SystemBus as _SystemBus
 
 
 # TODO: think of a better name
@@ -15,8 +13,8 @@ class SubprocessManager:
     def __init__(self):
         self.session_bus_available = True
         try:
-            self.bus = SessionBus()
-        except GError:
+            self.bus = _SessionBus()
+        except _GError:
             # No session bus if we're here. Depending on linux distro, that's
             # not surprising
             self.session_bus_available = False
@@ -25,7 +23,7 @@ class SubprocessManager:
         # systemd, which means that this next call will fail. Should probably
         # have a try/catch and then just default to not having a subprocess
         # manager if that happens.
-        self.system_bus = SystemBus()
+        self.system_bus = _SystemBus()
 
         # TODO: Verify that we can start services as the system bus w/o root
         # permissions
