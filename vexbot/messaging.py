a6b69204a9c278a8b261be6fbc86ba9b20862620
Ben Hoff <hoff.benjamin.k@gmail.com>
2017-09-20 10:35:25 +0000

    fixed messaging issues

--- vexbot/messaging.py
+++ vexbot/messaging.py
@@ -5,9 +5,9 @@ import zmq.devices
 
 from vexbot import _get_default_port_config
 from vexbot.util.socket_factory import SocketFactory as _SocketFactory
-from vexbot.util.messaging import get_address as _get_address
+from vexbot.util.messaging import get_addresses as _get_addresses
 
-from vexmessage import create_vex_message, decode_vex_message
+from vexmessage import create_vex_message, decode_vex_message, Request
 
 
 class Messaging:
@@ -16,7 +16,6 @@ class Messaging:
         configuration
         decode
         yield
-        control
     """
 
     # TODO: add an `update_messaging` command
@@ -103,20 +102,22 @@ class Messaging:
         command_address = to_address(self.configuration['command_port'])
         self.command_socket = create_n_conn(zmq.ROUTER, command_address, bind=True)
 
-        request_address = to_address(self.configuration['request_port']
+        request_address = to_address(self.configuration['request_port'])
         self.request_socket = create_n_conn(zmq.DEALER,
                                             request_address,
                                             bind=True,
                                             socket_name='request socket')
 
         iter_ = self._socket_factory.iterate_multiple_addresses
-        sub_addresses = iter_(self.configuration['chatter_subscription_port']
+        sub_addresses = iter_(self.configuration['chatter_subscription_port'])
         # TODO: verify that this shouldn't be like a connect as the socket factory defaults to bind
         # subscription socket is a XPUB socket
-        self.subscription_socket = create_n_conn(zmq.XPUB,
-                                                 sub_addresses,
-                                                 bind=True,
-                                                 socket_name='subscription socket')
+        multiple_create = self._socket_factory.multiple_create_n_connect
+
+        self.subscription_socket = multiple_create(zmq.XPUB,
+                                                   sub_addresses,
+                                                   bind=True,
+                                                   socket_name='subscription socket')
 
         self._poller.register(self.command_socket, zmq.POLLIN)
         self._poller.register(self.control_socket, zmq.POLLIN)
@@ -176,3 +177,42 @@ class Messaging:
         return create_vex_message(target, self._service_name, type_, **contents)
 
 
+    # NOTE: this should be moved to `messaging.py` at some point
+    def handle_raw_command(self, message) -> Request:
+        addresses = _get_addresses(message)
+        # NOTE: there should be a blank string between
+        # the address piece and the message content, which is why
+        # we add one to get the correct `address_length`
+        address_length = len(addresses) + 1
+        # remove the address information from the message
+        message = message[address_length:]
+        # the command name is the first string in message
+        command = message.pop(0).decode('ascii')
+
+        # Respond to PING commands
+        if command == 'PING':
+            addresses.append(b'PONG')
+            self.command_socket.send_multipart(addresses)
+            return
+
+        # NOTE: Message format is [command, args, kwargs]
+        args = message.pop(0)
+        # FIXME: wrap in try/catch and handle gracefully
+        # NOTE: pickle is NOT safe
+        args = pickle.loads(args)
+        # need to see if we have kwargs, so we'll try and pop them off
+        try:
+            kwargs = message.pop(0)
+        # if we don't have kwargs, then we'll alias our args and pass an
+        # empty list in for our args instead.
+        except IndexError:
+            kwargs = args
+            args = []
+        else:
+            # FIXME: wrap in try/catch and handle gracefully
+            # NOTE: pickle is NOT safe
+            kwargs = pickle.loads(kwargs)
+        
+        # TODO: use better names, request? command?
+        request = Request(command, address, args=args, kwargs=kwargs)
+        return request
