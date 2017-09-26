de422437b2257c07c550d87f5d459be23605ffb5
Ben Hoff <hoff.benjamin.k@gmail.com>
2017-09-26 11:47:51 +0000

    renaming to observer

--- vexbot/command_managers.py
+++ vexbot/command_managers.py
@@ -11,77 +11,7 @@ from vexbot.subprocess_manager import SubprocessManager
 from vexbot.util.function_wrapers import (msg_list_wrapper,
                                           no_arguments)
 
-class CommandManager:
-    def __init__(self, messaging: 'vexbot.messaging:Messaging'):
-        # NOTE: commands is a dict of dicts and there is nested parsing
-        self.observers = {}
-        self._messaging = messaging
-        # self._commands['commands'] = self._cmd_commands
-
-    def register_command(self,
-                         command: str,
-                         function_or_dict: (_collections.Callable, dict)):
-        """
-        `func_or_nested` can either be a function or a dictionary
-        """
-        self._commands[command] = function_or_dict
-
-    def parse_commands(self, msg: Request):
-        command = msg.contents.get('command')
-
-        if not command:
-            return
-
-        args = msg.contents.get('args')
-
-        callback, command, args = self._get_callback_recursively(command, args)
-        msg.contents['parsed_args'] = args
-
-        if callback:
-            results = callback(msg)
-
-            if results:
-                self._messaging.send_response(target=msg.source,
-                                              original=command,
-                                              response=results)
-
-    def process_command(self, message: Request):
-        raise RuntimeError('Not Implemented')
-        # Do we want to do an observable system or callback system?
-        callback, command, args = gcr(message.command, message.args)
-
-        if callback:
-            results = callback(message)
-
-        return results
-
-    def _cmd_commands(self, msg: Request):
-        """
-        returns a list of all available commands
-        works recursively
-        """
-        def get_commands(d: dict):
-            """
-            recursive command
-            """
-            commands = []
-            if not isinstance(d, dict):
-                return commands
-
-            for k, v in d.items():
-                if isinstance(v, dict):
-                    # NOTE: recursive command
-                    stuff = get_commands(v)
-                    for s in stuff:
-                        commands.append(k + ' ' + s)
-                else:
-                    commands.append(k)
-            return commands
-
-        return get_commands(self._commands)
-
-
-class BotCommands(Observer):
+class BotObserver(Observer):
     def __init__(self, messaging, subprocess_manager: SubprocessManager):
         super().__init__()
         self.messaging = messaging
