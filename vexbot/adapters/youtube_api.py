8c964f8835d21d346c214a593ee8c70ec0c22895
Ben Hoff <hoff.benjamin.k@gmail.com>
2017-09-29 19:06:44 +0300

    wip update to youtube

--- vexbot/adapters/youtube_api.py
+++ vexbot/adapters/youtube_api.py
@@ -1,7 +1,4 @@
-import sys
 import os
-import atexit
-import signal
 import logging
 import asyncio
 import argparse
@@ -10,15 +7,8 @@ import tempfile
 import pkg_resources
 from time import sleep
 
-import sqlalchemy as _alchy
-from sqlalchemy.orm import relationship
-
-import zmq
-from zmq import ZMQError
-from vexmessage import decode_vex_message
-
-from vexbot.command_managers import AdapterCommandManager
-from vexbot.adapters.messaging import ZmqMessaging # flake8: noqa
+from vexbot.adapters.messaging import Messaging
+from vexbot.adapters.scheduler import Scheduler
 
 _GOOGLE_API_INSTALLED = True
 
@@ -35,7 +25,46 @@ if _GOOGLE_API_INSTALLED:
     from oauth2client.file import Storage
     from oauth2client.tools import run_flow, argparser
 
-VEXBOT_SETTINGS_NAME = 'youtube'
+class Youtube:
+    def __init__(self, **kwargs):
+        self.messaging = Messaging('youtube')
+        scope = ['https://www.googleapis.com/auth/youtube',
+                 'https://www.googleapis.com/auth/youtube.force-ssl',
+                 'https://www.googleapis.com/auth/youtube.readonly']
+        self.api = _youtube_authentication(client_secret_filepath, scope)
+
+    def run(self):
+        self.messaging.start()
+        parts = 'snippet'
+        livestream_response = self.api.liveBroadcasts().list(mine=True,
+                                                             part=parts,
+                                                             maxResults=1).execute()
+
+
+        live_chat_id = livestream_response.get('items')[0]['snippet']['liveChatId']
+
+        livechat_response = youtube_api.liveChatMessages().list(liveChatId=live_chat_id, part='snippet').execute()
+
+        next_token = livechat_response.get('nextPageToken')
+        polling_interval = livechat_response.get('pollingIntervalMillis')
+        polling_interval = _convert_to_seconds(polling_interval)
+        while True:
+            await asyncio.sleep(polling_interval)
+            part = 'snippet, authorDetails'
+            livechat_response = self.api.liveChatMessages().list(liveChatId=live_chat_id, part=part, pageToken=next_token).execute()
+
+            next_token = livechat_response.get('nextPageToken')
+            polling_interval = livechat_response.get('pollingIntervalMillis')
+            polling_interval = _convert_to_seconds(polling_interval)
+
+            for live_chat_message in livechat_response.get('items'):
+                snippet = live_chat_message['snippet']
+                if not bool(snippet['hasDisplayContent']):
+                    continue
+                message = snippet['displayMessage']
+                author = live_chat_message['authorDetails']['displayName']
+                self.messaging.send_chatter(author=author, message=message)
+
 
 async def _run(messaging, live_chat_messages, live_chat_id, ):
     command_manager = AdapterCommandManager(messaging)
@@ -55,102 +84,12 @@ async def _run(messaging, live_chat_messages, live_chat_id, ):
         await asyncio.sleep(1)
 
 
-def _handle_close(messaging, event_loop):
-    def inner(*args):
-        _send_disconnect(messaging)()
-        for task in asyncio.Task.all_tasks():
-            task.cancel()
-        event_loop.stop()
-    return inner
-
-
-def _send_disconnect(messaging):
-    def inner():
-        messaging.send_status('DISCONNECTED')
-    return inner
-
 
 def main(client_secret_filepath, publish_address, subscribe_address):
     if not _GOOGLE_API_INSTALLED:
         logging.error('`google-api-python-client` required to use youtube. Install using `pip install google-api-python-client')
         return
 
-    # TODO: Determine if this try/except pattern has become outdated
-    # with new `connect` methods being called rather than the old bind
-    try:
-        messaging = ZmqMessaging('youtube',
-                                 publish_address,
-                                 subscribe_address,
-                                 'youtube')
-
-        messaging.start_messaging()
-    except ZMQError:
-        return
-
-    # signal.signal(signal.SIGINT, handle_close)
-    # signal.signal(signal.SIGTERM, handle_close)
-    # handle_close = _handle_close(messaging)
-
-    scope = ['https://www.googleapis.com/auth/youtube',
-             'https://www.googleapis.com/auth/youtube.force-ssl',
-             'https://www.googleapis.com/auth/youtube.readonly']
-
-    youtube_api = _youtube_authentication(client_secret_filepath, scope)
-    parts = 'snippet'
-    livestream_response = youtube_api.liveBroadcasts().list(mine=True,
-                                                            part=parts,
-                                                            maxResults=1).execute()
-
-
-    live_chat_id = livestream_response.get('items')[0]['snippet']['liveChatId']
-
-    livechat_response = youtube_api.liveChatMessages().list(liveChatId=live_chat_id, part='snippet').execute()
-
-    next_token = livechat_response.get('nextPageToken')
-    polling_interval = livechat_response.get('pollingIntervalMillis')
-    polling_interval = _convert_to_seconds(polling_interval)
-    messaging.send_status('CONNECTED')
-
-    event_loop = asyncio.get_event_loop()
-    asyncio.ensure_future(_recv_loop(messaging,
-                                     youtube_api,
-                                     live_chat_id,
-                                     next_token,
-                                     polling_interval))
-
-    asyncio.ensure_future(_run(messaging,
-                               youtube_api.liveChatMessages(),
-                               live_chat_id))
-
-    atexit.register(_send_disconnect(messaging))
-    event_loop.run_forever()
-
-
-    messaging.send_status('DISCONNECTED')
-
-
-async def _recv_loop(messaging,
-                     youtube_api,
-                     live_chat_id,
-                     next_token,
-                     polling_interval):
-
-    while True:
-        await asyncio.sleep(polling_interval)
-        part = 'snippet, authorDetails'
-        livechat_response = youtube_api.liveChatMessages().list(liveChatId=live_chat_id, part=part, pageToken=next_token).execute()
-
-        next_token = livechat_response.get('nextPageToken')
-        polling_interval = livechat_response.get('pollingIntervalMillis')
-        polling_interval = _convert_to_seconds(polling_interval)
-
-        for live_chat_message in livechat_response.get('items'):
-            snippet = live_chat_message['snippet']
-            if not bool(snippet['hasDisplayContent']):
-                continue
-            message = snippet['displayMessage']
-            author = live_chat_message['authorDetails']['displayName']
-            messaging.send_message(author=author, message=message)
 
 def _convert_to_seconds(milliseconds: str):
     return float(milliseconds)/1000.0
@@ -169,7 +108,7 @@ def _get_kwargs():
 
 _YOUTUBE_API_SERVICE_NAME = 'youtube'
 _YOUTUBE_API_VERSION = 'v3'
-_READ_ONLY = "https://www.googleapis.com/auth/youtube.readonly"
+_ALL = "https://www.googleapis.com/auth/youtube"
 
 
 def _youtube_authentication(client_filepath, youtube_scope=_READ_ONLY):
