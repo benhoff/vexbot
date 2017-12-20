0f5dfbb888d3bcb01ef9867940f9a66392859324
Ben Hoff <hoff.benjamin.k@gmail.com>
2017-12-20 20:07:46 +0000

    added in digital ocean

---
+++ vexbot/extensions/digitalocean.py
@@ -0,0 +1,36 @@
+import os
+import digitalocean
+
+
+_MANAGER = False
+_DEFAULT_DROPLET = os.getenv('DIGITAL_OCEAN_DROPLET_ID')
+_DIGITAL_OCEAN_KEY = os.getenv('DIGITAL_OCEAN_KEY')
+if _DIGITAL_OCEAN_KEY:
+    _MANAGER = digitalocean.Manager(token=_DIGITAL_OCEAN_KEY)
+
+
+def get_size(*args, **kwargs):
+    id_ = kwargs.get('id', _DEFAULT_DROPLET)
+    if id_ is None:
+        # FIXME: implement a way to pass this in as a config
+        raise RuntimeError('Must pass in an `id_` or set the `DITIAL_OCEAN_DROPLET_ID` in your envirnoment')
+    droplet = _MANAGER.get_droplet(id_)
+    return droplet.size
+
+
+def power_off(*args, **kwargs):
+    id_ = kwargs.get('id', _DEFAULT_DROPLET)
+    if id_ is None:
+        # FIXME: implement a way to pass this in as a config
+        raise RuntimeError('Must pass in an `id_` or set the `DITIAL_OCEAN_DROPLET_ID` in your envirnoment')
+    droplet = _MANAGER.get_droplet(id_)
+    droplet.power_off()
+
+
+def power_on(*args, **kwargs):
+    id_ = kwargs.get('id', _DEFAULT_DROPLET)
+    if id_ is None:
+        # FIXME: implement a way to pass this in as a config
+        raise RuntimeError('Must pass in an `id_` or set the `DITIAL_OCEAN_DROPLET_ID` in your envirnoment')
+    droplet = _MANAGER.get_droplet(id_)
+    droplet.power_on()
